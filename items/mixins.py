9eba3618a996e0c51577ec9fd0fe255d3accddd0
raphael apeh <raphaelapeh37@gmail.com>
2025-02-21 21:27:53 +0100

    added search mixin

---
+++ items/mixins.py
@@ -0,0 +1,32 @@
+import inspect
+
+from django.db.models.query import Q
+
+
+class ModelSearchMixin:
+
+    search_fields = None
+    query_param = "q"
+
+    def get_search_fields(self):
+        
+        assert isinstance(self.search_fields, (tuple, list)), "'search_fields' must be of type tuple or list"
+        return self.search_fields
+    
+    def search_field(self, queryset):      
+        
+        search_lookup = self.request.GET.get(self.query_param, None)
+        
+        assert search_lookup is not None, "'query_param' can't be of type None"
+        query = Q()
+        for field in self.get_search_fields():
+            query |= Q(**{f"{field}__icontains": search_lookup.strip()})
+
+        return queryset.filter(query).distinct()
+    
+    def get_queryset(self):
+        queryset = super().get_querset()
+        
+        if self.search_fields and self.request.GET.get(self.query_param):
+            return self.search_field(queryset)
+        return queryset

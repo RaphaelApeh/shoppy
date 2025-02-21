c08bb4cad10dc5d44fba3f21f0fca24b835135ca
raphael apeh <raphaelapeh37@gmail.com>
2025-02-21 22:12:44 +0100

    renamed the query_param

--- items/views.py
+++ items/views.py
@@ -11,4 +11,4 @@ class ItemListView(ModelSearchMixin, ListView):
     template_name = "items/items_list.html"
     queryset = Item.objects.all()
     search_fields = ["quantities", "user__username", "tags__name", "name"]
-    query_param = "apeh"
+    

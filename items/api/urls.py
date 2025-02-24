99cb85a578fce6b2f5262998e00ca6272659f520
raphael apeh <raphaelapeh37@gmail.com>
2025-02-24 22:57:50 +0100

    added user items viewset

--- items/api/urls.py
+++ items/api/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
+from rest_framework.routers import DefaultRouter
 
 from . import views
 
 urlpatterns = [
     path("items/", views.ItemListView.as_view())
 ]
+
+router = DefaultRouter()
+router.register("user-items", views.UserItemView)
+urlpatterns += router.get_urls()
--- items/api/views.py
+++ items/api/views.py
@@ -1,11 +1,15 @@
 from django_filters import rest_framework as filters
-from rest_framework import generics
+from rest_framework import (
+    generics,
+    permissions,
+    viewsets
+)
 
 from items.models import Item
 from items.filters import ItemFilter
 
 from .serializers import ItemSerializer
-
+from .permissions import IsOwnerOrIsStaff
 
 class ItemListView(generics.ListAPIView):
     """
@@ -13,6 +17,18 @@ class ItemListView(generics.ListAPIView):
     """
 
     serializer_class = ItemSerializer
-    queryset = Item.objects.select_related("user")
+    queryset = Item.objects.select_related("user", "user__profile")
     filter_backends = [filters.DjangoFilterBackend]
     filterset_class = ItemFilter
+    permission_classes = [permissions.IsAdminUser]
+
+
+class UserItemView(viewsets.ModelViewSet):
+
+    queryset = Item.objects.select_related("user", "user__profile")
+    serializer_class = ItemSerializer
+    permission_classes = [IsOwnerOrIsStaff, permissions.IsAuthenticated]
+    pagination_class = None
+
+    def get_queryset(self):
+        return super().get_queryset().filter(user=self.request.user)

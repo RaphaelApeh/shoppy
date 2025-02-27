ec4c824b4f657e14035d49c418d30b7a90119cc4
raphael apeh <raphaelapeh37@gmail.com>
2025-02-27 23:01:41 +0100

    add datail view template

--- items/urls.py
+++ items/urls.py
@@ -5,6 +5,11 @@ from . import views
 app_name: str = "items"
 
 urlpatterns: list[path] = [
-    path("items/", views.ItemListView.as_view())
+    path("items/", views.ItemListView.as_view(), name="items_list"),
+    path(
+        "items/<str:slug>",
+        views.ItemDetailView.as_view(),
+        name="items_detail"
+    )
 
 ]
--- items/views.py
+++ items/views.py
@@ -3,6 +3,7 @@ from django.views.generic import (
     CreateView,
     DetailView
 )
+from django.db import connection
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .mixins import ModelSearchMixin
@@ -24,11 +25,17 @@ class ItemDetailView(DetailView):
     template_name = "items/items_detail.html"
     model = Item
     queryset = Item.objects.select_related("user")
+    
+    def get_queryset(self):
+        qs = super().get_queryset()
+        print(connection.queries)
+        return qs
 
     def get_context_data(self, **kwargs):
 
         context = super().get_context_data(**kwargs)
-        context["user_related_items"] = self.get_queryset().user_items(self.get_object().user)
+        obj = self.get_object()
+        context["user_related_items"] = self.get_queryset().user_items(obj.user).exclude(pk=obj.pk)
 
         return context
 

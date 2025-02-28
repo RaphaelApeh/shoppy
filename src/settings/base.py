8ca2ea6a619cdf4d2b83dd225f8ec586d971d82a
raphael apeh <raphaelapeh37@gmail.com>
2025-02-28 19:32:22 +0100

    added some allauth settings

--- src/settings/base.py
+++ src/settings/base.py
@@ -158,3 +158,7 @@ REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 5
 }
+
+# Allauth
+ACCOUNT_EMAIL_VERIVICATION = "mandatory"
+ACCOUNT_EMAIL_REQUIRED = True

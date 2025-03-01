b52dc824721181fcd02ea264edf07028dead5cb8
raphael apeh <raphaelapeh37@gmail.com>
2025-03-01 20:36:38 +0100

    added user profile

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,13 @@
 from django.urls import path, include
 
+from . import views
+
 urlpatterns = [
-    path("", include("allauth.urls"))
+    path("", include("allauth.urls")),
+
+    path(
+        "profile/",
+        views.UserProfile.as_view(),
+        name="account_profile"
+    )
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,13 @@
+from django.views.generic import View
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 
-# Create your views here.
+
+class UserProfile(
+    LoginRequiredMixin,
+    View
+    ):
+    
+    def get(self, request):
+    
+        return render(request, "accounts/profile.html")
